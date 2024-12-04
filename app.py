ed6fcff24f1960ef6ffa946c3400f4e12984dfff
DM85 <DM3177504166@OUTLOOK.com>
2024-12-04 22:05:33 +0800

    获取各地市高企的产业数据

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import csv
 import io
 import os
 import re
-
 import pandas as pd
 import requests
 from flask import Flask, request, redirect, url_for, render_template, flash
@@ -13,19 +12,28 @@ from openpyxl.reader.excel import load_workbook
 
 app = Flask(__name__)
 
-HDFS_HOST = 'http://121.36.46.125:9870'
+HOST = "121.36.46.125"
+HDFS_URL = 'http://121.36.46.125:9870'
 HDFS_NAMENODE_HOST = 'http://121.36.46.125:9870/webhdfs/v1'
 HDFS_USER = 'root'
 HDFS_UPLOAD_DIR = '/user/data/'
+HIVE_PORT = '10000'
+
+areas = {'4501': '南宁', '4502': '柳州', '4503': '桂林', '4504': '梧州', '4505': '北海', '4506': '防城港',
+         '4507': '钦州', '4508': '贵港', '4509': '玉林', '4510': '百色', '4511': '贺州', '4512': '河池', '4513': '来宾',
+         '4514': '崇左'}
+
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     return render_template('upload.html')
 
+
 # 上传文件到HDFS，从HDFS获取文件进行清洗后导入hive数据仓库
 @app.route("/upload", methods=["POST", "GET"])
 def upload():
-    if request.method == 'POST':    # 1.处理上传的文件
+    if request.method == 'POST':
+        # 1.处理上传的文件
         # 判断请求中是否包含文件
         if 'file' not in request.files:
             a_json = {"result": "没有文件部分", "status": 400}
@@ -45,7 +53,7 @@ def upload():
         # 第一个工作表
         sheet = workbook.active
         # 将文件转化为csv文件
-        # 准备CSV文件名（可以根据需要修改）
+        # 准备CSV文件名
         csv_filename = os.path.join('高企源数据.csv')
         # 写入CSV文件
         with open(csv_filename, mode='w', newline='', encoding='utf-8') as csv_file:
@@ -53,7 +61,7 @@ def upload():
             header = [cell.value for cell in sheet[1]]  # 第一行作为表头
             writer.writerow(header)
             # 写入数据行
-            for row in sheet.iter_rows(min_row=2,values_only=True):
+            for row in sheet.iter_rows(min_row=2, values_only=True):
                 writer.writerow(row)
 
         # 3.将源数据上传到HDFS
@@ -71,8 +79,8 @@ def upload():
         # 删除列名为“数据状态”且其值为“dis”的行
         df = df[df['数据状态'] != 'dis']
         # 删除 序号 数据年份 数据状态 撤销原因 申请撤销状态 管理员审核状态 错误数量  是否填写国家统计局一套表
-        columns_to_drop = ['序号', '帐号', '数据年份', '数据状态', '撤消原因', '申请撤消状态', '管理员审核状态', '错误数量',
-                           '警告数量', '数据提交状态', '是否填写国家统计局一套表']
+        columns_to_drop = ['序号', '帐号', '数据年份', '数据状态', '撤消原因', '申请撤消状态', '管理员审核状态',
+                           '错误数量', '警告数量', '数据提交状态', '是否填写国家统计局一套表']
         df = df.drop(columns=columns_to_drop)
         # 填充空值
         df.fillna(-2147483640, inplace=True)
@@ -80,8 +88,6 @@ def upload():
         # 除掉列名中的带圈序号
         # 除掉列名中的逗号
         df.rename(columns=lambda col: re.sub(r'[，：.:,（）()]', '', col), inplace=True)
-        # 删除2-4行
-        df = df.iloc[:1].append(df.iloc[2:]).reset_index(drop=True)
 
         # 保存回一个新的CSV文件
         cleanDataTableName = 'clean_data.csv'
@@ -91,6 +97,39 @@ def upload():
         # 根据清洗过后的表头在hive建表
         importHive(cleanDataTableName)
     return render_template('upload.html', ok='ok')
+
+
+# 分析各地市高企的产业分布情况
+@app.route("/chanYe")
+def chanYe():
+    # 1需要的数据有：cleanData表里的行政区化代码、行业代码、code表里的产业名
+    cursor, conn = connectHive()
+    cursor.execute(
+        'SELECT cd.`行政区划代码`,c.`行业名`,COUNT(*) AS `行业数量` FROM cleanData AS cd JOIN code AS c ON cd.`行业代码2017` = c.`行业代码` group by cd.`行政区划代码`, c.`行业名`')
+    # cursor.execute('select `行业名` from code')
+    results = cursor.fetchall()
+    print(results)
+    a_json = {"result": results, "status": 200}
+    print(a_json['result'])
+    closeHive(cursor, conn)
+    return json.dumps(a_json, ensure_ascii=False)
+
+
+# 连接hive的函数
+def connectHive():
+    conn = connect(host="121.36.46.125", port=10000, user="root", password="Hadoop123456",
+                   auth_mechanism="PLAIN")
+    print("1")
+    # 连接后，获取游标，标志指示数据库的当前记录
+    cursor = conn.cursor()
+    return cursor, conn
+
+
+# 关闭hive的函数
+def closeHive(cursor, conn):
+    cursor.close()
+    conn.close()
+
 
 # 测试可不可根据表头信息自动建表，字段太多了，无法手动建表
 def importHive(tableName):
@@ -131,7 +170,10 @@ def importHive(tableName):
             result += ','
         result += f"`{key}` {value}"
         flat = True
-    print(result)
+
+    # 删除0-3行
+    df = df.iloc[:0].append(df.iloc[3:]).reset_index(drop=True)
+    df.to_csv(tableName, index=False)
 
     conn = connect(host="121.36.46.125", port=10000, user="root", password="Hadoop123456",
                    auth_mechanism="PLAIN")
@@ -145,9 +187,8 @@ def importHive(tableName):
 
     # 3.将文件上传到HDFS
     # 创建HDFS客户端
-    # 删除表头和前三行
-    df.to_csv(tableName)
-    client = InsecureClient(HDFS_HOST, user=HDFS_USER)
+
+    client = InsecureClient(HDFS_URL, user=HDFS_USER)
     # 上传文件到HDFS
     hdfs_path = HDFS_UPLOAD_DIR + tableName
     print(hdfs_path)
@@ -155,15 +196,6 @@ def importHive(tableName):
     cursor = conn.cursor()
     cursor.execute(f"LOAD DATA INPATH '{hdfs_path}' INTO TABLE {table_name} ")
     print("2")
-
-
-
-
-
-
-
-
-
 
 
 # 测试上传本地文件到hdfs
@@ -220,7 +252,7 @@ def uploadHive():
     df.to_csv('属性表.csv', index=False)
     # 3.将文件上传到HDFS
     # 创建HDFS客户端
-    client = InsecureClient(HDFS_HOST, user=HDFS_USER)
+    client = InsecureClient(HDFS_URL, user=HDFS_USER)
     # 上传文件到HDFS
     client.upload('/user/data/clean_data.csv', '属性表.csv')
     # 4.创建表（我已经创建好了）
@@ -246,7 +278,6 @@ def link():
     print(results)
     return json.dumps(a_json, ensure_ascii=False)
     # return "ok"
-
     # return json.dumps({"result": "网络连接错误", "status": 404}, ensure_ascii=False)
 
 
